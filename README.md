afb4b5522de2984b75b2633c48a0d71f56d0e87a
itproto <itprotoltd@gmail..com>
2023-09-21 22:26:36 +0100

    3. Publish react app under server/public and mount in express for deployment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Getting Started with Create React App
 TODO: Add description
+
+AWS:
+1. Goto https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#Home to lunch EC2
+2. Select AWS Linux 2 AMI (HVM)/ T2 mico
+3. Create security group
+4. Connect https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#ConnectToInstance:instanceId=i-07dfc127bbfb1f9d1
+5. Access to instance
--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const cors = require('cors');
 const expressWs = require('express-ws');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const userRoutes = require('./apiRoutes/user');
 app.use('/api', userRoutes);
@@ -16,7 +21,7 @@ expressWs(app);
 const gameRouter = require('./apiRoutes/game');
 app.use('/api', gameRouter);
 
-const port = 3002 || process.env.PORT;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
     console.log(`server listening on port ${port}`);
 })
